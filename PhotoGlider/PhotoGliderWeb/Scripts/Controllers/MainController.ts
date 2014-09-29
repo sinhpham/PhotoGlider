ed8fa5883f76f3bb8885b72baeba3070d15b26dd
Sinh Pham <phamansinh@gmail.com>
2014-09-29 02:50:56 -0600

    Indefinite scrolling prototype.

--- PhotoGlider/PhotoGliderWeb/Scripts/Controllers/MainController.ts
+++ PhotoGlider/PhotoGliderWeb/Scripts/Controllers/MainController.ts
@@ -2,18 +2,26 @@
 
 module Main {
     export interface Scope {
-        images: string[]
+        images: string[];
+        loadMore: () => void;
     }
 
     export class Controller {
         constructor(private $scope: Scope, private $http: ng.IHttpService, private $q: ng.IQService) {
             $scope.images = ["1", "2", "3"];
-            this.getInitialPage();
+            $scope.loadMore = this.loadMore;
+            this.after = "";
+            var a = 0;
+            //this.getInitialPage();
         }
+
+        busy: boolean;
+        after: string;
 
         getInitialPage() {
             var deferred = this.$q.defer();
             var scope = this.$scope;
+            var currAfter = this.after;
             ///blah
             return this.$http.get('http://www.reddit.com/r/all/new.json?limit=50')
                 .success(function (data: any) {
@@ -30,7 +38,8 @@ module Main {
 
                             return v.data.thumbnail;
                         });
-                    console.log(data);
+                    currAfter = data.data.after;
+                    var a = 0;
                 })
                 .error(function (data) {
                     deferred.reject(data);
@@ -38,15 +47,87 @@ module Main {
             return deferred.promise;
         }
 
-        //loadMore() {
-        //    var last = this.$scope.images[this.$scope.images.length - 1];
-        //    for (var i = 1; i <= 8; i++) {
-        //        this.$scope.images.push(last + i);
-        //    }
-        //    var a = 0;
-        //}
+        loadMore() {
+            if (this.busy) return;
+            this.busy = true;
+            var url = "";
+            if (this.after) {
+                url = "http://www.reddit.com/r/all/new.json?after=" + this.after
+            } else {
+                url = "http://www.reddit.com/r/all/new.json";
+            }
+
+
+            var deferred = this.$q.defer();
+            var scope = this.$scope;
+
+            return this.$http.get(url)
+                .success(function (data: any) {
+                    var count = data.data.children.length;
+                    deferred.resolve(data);
+                    var dataArr = <Array<any>>data.data.children;
+                    var newImages = dataArr.filter((v, idx, arr) => {
+                        var tLink = v.data.thumbnail;
+                        if (tLink == "" || tLink == "self" || tLink == "nsfw" || tLink == "default") {
+                            return false;
+                        }
+                        return true;
+                    }).map((v, idx, arr) => {
+
+                            return v.data.thumbnail;
+                        });
+                    newImages.forEach(img => {
+                        scope.images.push(img);
+                    });
+
+                })
+                .error(function (data) {
+                    deferred.reject(data);
+                });
+            return deferred.promise;
+        }
     }
 }
 
-angular.module('PhotoGliderWeb', []);
-//angular.module('', ['infinite-scroll']);
+angular.module('PhotoGliderWeb', ['infinite-scroll']);
+
+angular.module('myApp', ['infinite-scroll']);
+
+module myApp {
+    export interface Scope {
+        reddit: Reddit
+    }
+
+    export class Reddit {
+        constructor(private $http: ng.IHttpService) {
+            this.items = [];
+            this.busy = false;
+            this.after = '';
+        }
+
+        nextPage() {
+            if (this.busy) return;
+            this.busy = true;
+
+            var url = "http://api.reddit.com/hot?after=" + this.after + "&jsonp=JSON_CALLBACK";
+            this.$http.jsonp(url).success(function (data) {
+                var items = data.data.children;
+                for (var i = 0; i < items.length; i++) {
+                    this.items.push(items[i].data);
+                }
+                this.after = "t3_" + this.items[this.items.length - 1].id;
+                this.busy = false;
+            }.bind(this));
+        }
+
+        items: string[];
+        busy: boolean;
+        after: string;
+    }
+
+    export class DemoController {
+        constructor($scope: Scope, $http: ng.IHttpService) {
+            $scope.reddit = new Reddit($http);
+        }
+    }
+}
