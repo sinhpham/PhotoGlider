69b74bc8e2ccde5ee26d0cc254ca73b547d76c71
Sinh Pham <phamansinh@gmail.com>
2014-10-18 18:25:18 -0600

    Common + mainpage prototype.

---
+++ PhotoGliderWindowsStore/Common/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace PhotoGliderWindowsStore.Common
+{
+    public static class Utils
+    {
+        public static T Deserialize<T>(string json)
+        {
+            var bytes = Encoding.Unicode.GetBytes(json);
+            using (var stream = new MemoryStream(bytes))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                return (T)serializer.ReadObject(stream);
+            }
+        }
+
+        public static string Serialize(object instance)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(instance.GetType());
+                serializer.WriteObject(stream, instance);
+                stream.Position = 0;
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible;
+        }
+    }
+}
