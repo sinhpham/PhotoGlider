e0a5e887a61bbefc836ee7346abea5e23392e82b
Sinh Pham <phamansinh@gmail.com>
2014-10-21 17:10:22 -0600

    WP project.

---
+++ PhotoGliderWP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhotoGliderWP.Resources;
+
+namespace PhotoGliderWP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
