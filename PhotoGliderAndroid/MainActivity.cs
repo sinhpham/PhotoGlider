307d1d3a63a4b0047dbb53963f69ad7a9f1ab937
Sinh Pham <phamansinh@gmail.com>
2014-10-21 12:59:18 -0600

    iOS and Android projects.

---
+++ PhotoGliderAndroid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using PhotoGliderPCL;
+using Xamarin.Forms.Platform.Android;
+
+namespace PhotoGliderAndroid
+{
+    [Activity(Label = "PhotoGliderAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AndroidActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Forms.Init(this, bundle);
+
+            SetPage(AppPCL.GetMainPage());
+        }
+    }
+}
+
+
